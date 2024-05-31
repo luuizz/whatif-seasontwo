ef3009da09acd3dd5e84363002f820fe97a67fb7
luuizz <luu.izz@hotmail.com>
2024-05-31 20:45:17 -0300

    fix: corrigido escapamento

--- src/components/Home/SectionHero/index.js
+++ src/components/Home/SectionHero/index.js
@@ -13,7 +13,7 @@ export default function HomeSectionHero() {
         <div className="main">
           <div className={`${contentStyleHero} left`}>
             <h3>Segunda temporada</h3>
-            <h1>Marvel's WHAT IF... ?</h1>
+            <h1>Marvels WHAT IF... ?</h1>
             <p>Disponível no Disney+. What If...? é uma série de animação que leva o público para um terreno desconhecido ao contar de uma forma alternativa, momentos cruciais do Universo Cinematográfico Marvel, imaginando o que teria acontecido aos super-heróis fosse totalmente diferente e se passasse em outra realidade.</p>
             <Btn 
             url="https://www.disneyplus.com/pt-br/series/what-if/7672ZVj1ZxU9" 
