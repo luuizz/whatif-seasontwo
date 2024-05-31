10709e783db93d0a134d98d375e37c33d9e79a10
luuizz <luu.izz@hotmail.com>
2024-05-31 20:44:38 -0300

    feat: adicionado listagem

--- src/app/personagens/[id]/page.js
+++ src/app/personagens/[id]/page.js
@@ -1,11 +1,12 @@
+import HomeSectionTodosPersonagens from '@/components/Home/SectionAllCharacters'
 import TopDetailsSingle from '@/components/SinglePost/TopDetails'
 import React from 'react'
 
 export default function PagePersonagem({params}) {
   return (
-    <div>
-        <h1>Personagem {params.id}</h1>
+    <>
         <TopDetailsSingle />
-    </div>
+        <HomeSectionTodosPersonagens />
+    </>
   )
 }
