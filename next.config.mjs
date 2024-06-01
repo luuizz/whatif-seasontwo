57452fc4d3afc17121778215eef0404ad49a3857
luuizz <luu.izz@hotmail.com>
2024-06-01 14:14:55 -0300

    feat: habilitado para aceitar e minifcar as imagens vindas da API

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,11 @@ import { createVanillaExtractPlugin } from '@vanilla-extract/next-plugin';
   const withVanillaExtract = createVanillaExtractPlugin();
   
   /** @type {import('next').NextConfig} */
-  const nextConfig = {};
+  const nextConfig = {
+    swcMinify: true,
+    images: {
+      domains: ["whatif-seasontwo.cdn.prismic.io"],
+    }
+  };
   
   export default withVanillaExtract(nextConfig);
