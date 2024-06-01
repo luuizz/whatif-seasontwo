a7b5428f417c0eb0998b45b521231eafd7a9e74e
luuizz <luu.izz@hotmail.com>
2024-06-01 10:12:45 -0300

    feat: adicionado componente que adiciona as animações

--- src/app/personagens/[id]/page.js
+++ src/app/personagens/[id]/page.js
@@ -1,12 +1,15 @@
 import HomeSectionTodosPersonagens from '@/components/Home/SectionAllCharacters'
 import TopDetailsSingle from '@/components/SinglePost/TopDetails'
 import React from 'react'
+import PageWrapper from './PageWrapper'
 
 export default function PagePersonagem({params}) {
   return (
     <>
+    <PageWrapper>
         <TopDetailsSingle />
         <HomeSectionTodosPersonagens />
+    </PageWrapper>
     </>
   )
 }
