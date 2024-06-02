44db5d44a8ff9eef1dc34c20ce59c24854602618
luuizz <luu.izz@hotmail.com>
2024-06-02 14:15:10 -0300

    feat: refatorado código e responsivado o rodapé

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,17 +1,17 @@
 import Image from "next/image";
 import LogoCodeboost from "@/assets/logo-codeboost.svg"
-import { styleFooter } from "./style.css";
+import { areaLeftFooterStyle, containerFooterStyle, spanFooterStyle, styleFooter, textFooterStyle } from "./style.css";
 
 export default function Footer() {
   const year = new Date().getFullYear();
 
   return (
     <footer className={styleFooter}>
-        <div className="container">
-            <div className="left">
-              <p>©{year} CodeBoost</p>
+        <div className={`${containerFooterStyle} container`}>
+            <div className={`${areaLeftFooterStyle} left`}>
+              <p className={textFooterStyle}>©{year} CodeBoost</p>
 
-              <span>O curso Codeboost foi desenvolvido por William Moreira. As vagas são distribuídas por turmas de forma sazonal. Caso perca alguma oportunidade é só aguardar a próxima turma que normalmente acontece dentro do período de 3 a 4 meses.</span>
+              <span className={spanFooterStyle}>O curso Codeboost foi desenvolvido por William Moreira. As vagas são distribuídas por turmas de forma sazonal. Caso perca alguma oportunidade é só aguardar a próxima turma que normalmente acontece dentro do período de 3 a 4 meses.</span>
             </div>
 
             <div className="right">
--- src/components/Footer/style.css.js
+++ src/components/Footer/style.css.js
@@ -3,25 +3,64 @@ import { style } from "@vanilla-extract/css";
 export const styleFooter = style({
     borderTop: "1px solid rgba(255,255,255,0.2)",
     padding: "4rem 0",
-    ".container": {
-        display: "flex",
-        alignItems: "flex-start",
-        justifyContent: "space-between",
-        ".left": {
-            display: "flex",
-            alignItems: "flex-start",
-            "p": {
-                fontSize: "1.4rem",
-                fontWeight: 500,
-            },
-            "span": {
-                flex: 1,
-                fontSize: "1.4rem",
-                color: "var(--gray-500)",
-                lineHeight: "2.1rem",
-                maxWidth: "59.3rem",
-                marginLeft: "18.6rem"
-            }
+})
+
+export const containerFooterStyle = style({
+    display: "flex",
+    alignItems: "flex-start",
+    justifyContent: "space-between",
+    '@media': {
+        '(max-width: 1300px)': {
+            padding: "0 15px",
+        },
+        '(max-width: 991px)': {
+            flexDirection: "column",
+            alignItems: "center",
+        }
+    }
+})
+
+export const areaLeftFooterStyle = style({
+    display: "flex",
+    alignItems: "flex-start",
+    '@media': {
+        '(max-width: 991px)': {
+            flexDirection: "column",
+            alignItems: "center",
+            marginBottom: "3.2rem",
+        },
+    }
+})
+
+export const textFooterStyle = style({
+    fontSize: "1.4rem",
+    fontWeight: 500,
+    '@media': {
+        '(max-width: 991px)': {
+            marginBottom: "2.4rem"
+        },
+        '(max-width: 560px)': {
+            textAlign: "center",
+            maxWidth: "100%",
+            width: "100%",
+        },
+    }
+})
+
+export const spanFooterStyle = style({
+    flex: 1,
+    fontSize: "1.4rem",
+    color: "var(--gray-500)",
+    lineHeight: "2.1rem",
+    maxWidth: "59.3rem",
+    marginLeft: "18.6rem",
+    '@media': {
+        '(max-width: 1200px)': {
+            marginLeft: "6.6rem",
+        },
+        '(max-width: 991px)': {
+            marginLeft: 0,
+            textAlign: "center",
         },
     }
 })
